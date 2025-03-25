24a98ea329ae318ca3bb6e3ee085310844d8a326
AditiSethi0419 <sethiaditi055@gmail.com>
2025-03-25 19:07:35 +0530

    added usestate functionality

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from "./style.module.css";
 
-const RestaurantCard = () => {
+
+const RestaurantCard = (props) => {
+  const { info } = props.resData;
+
   return (
     <div className={styles['res-card']}>
-        <div className={styles["image-container"]}>
-            <img className={styles["item-img"]} />
-            <p className={styles["item-price"]}>Items at 129</p>
-        </div>
-        <div className={styles["hotel-info"]}>
-            <p className={styles["hotel-name"]}>Satkar Restaurant</p>
-            <div className={styles['hotel-ratings']}>4.4 * 40-45 mins</div>
-            <p className={styles['hotel-cuisines']}>North Indian, South Indian, Indian, Salads, Desserts
-            Satkar Chowk</p>
-        </div>
+      <div className={styles["image-container"]}>
+        <img className={styles["item-img"]} src={'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/' + info?.cloudinaryImageId} />
+        <p className={styles["item-price"]}> ITEMS {info?.aggregatedDiscountInfoV3.subHeader} </p>
+      </div>
+      <div className={styles["hotel-info"]}>
+        <p className={styles["hotel-name"]}>{info?.name}</p>
+        <div className={styles['hotel-ratings']}>{info?.avgRating} * {info?.sla.deliveryTime} mins</div>
+        <p className={styles['hotel-cuisines']}>
+          {info?.cuisines.map(cuisine =>
+            cuisine.replace(/\b\w/g, char => char.toUpperCase())
+          ).join(", ")}
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import RestaurantCard from '../../components/RestaurantCard';
+import React, { useState } from "react";
+import RestaurantCard from "../../components/RestaurantCard";
 import styles from "./style.module.css";
+import resList from "../../utils/data";
 
 const Home = () => {
+  const [listOfRestaurants , setListOfRestaurants] = useState(resList);
   return (
-    <div className={styles['top-res-sec']}>
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
+    <div className="cards-wrap">
+      <div className="filter">
+        <button className="filter-btn" onClick={()=>{
+        const filteredList = listOfRestaurants.filter(
+          (res) => res.info.avgRating > 4
+        );
+        setListOfRestaurants(filteredList);
+        }}>
+          Top Rated Restaurants
+        </button>
+
+      </div>
+        <div className={styles["top-res-sec"]}>
+      {listOfRestaurants.map((restaurant) => (
+        <RestaurantCard resData={restaurant} key={restaurant.info.id}/>
+      ))}
+      </div>
     </div>
+  
   );
-}
+};
 
 export default Home;
